744cc9fd3b5487d6b7a067c85a7d64fa34a13bfd
salmansaifeldin <75371984+salmansaifeldin@users.noreply.github.com>
2025-02-16 16:56:27 +0200

    Create GlowingText.jsx

---
+++ src/components/GlowingText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GlowingText = () => {
+  return (
+    <h1 className="text-6xl font-bold text-center text-blue-400 glow">
+      VEB SECURITY
+    </h1>
+  );
+};
+
+export default GlowingText;
